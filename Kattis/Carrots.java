607ba234fa2e575e494a6f48d7f4d3f9d255da71
Zamin Ali Baltee <zaminali7865@gmail.com>
2022-11-14 13:56:45 +0500

    committed carrot

---
+++ Kattis/Carrots.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Carrots {
+    public static void main(String [] args) throws IOException{ 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int contestent = Integer.parseInt(st.nextToken());
+        int probSolved = Integer.parseInt(st.nextToken());
+        while(contestent -- > 0){
+            String str = br.readLine();
+        }
+        pw.printf("%d \n", probSolved);
+        pw.flush();
+        br.close();
+        pw.close();
+
+
+    }
+}
